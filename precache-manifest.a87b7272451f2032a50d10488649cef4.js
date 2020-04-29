045f2eba1dd729d6726c7515ccfdf60d8753b8d2
blooot <dalehamilton@trentu.ca>
2020-04-29 04:37:01 -0700

    Updates

---
+++ precache-manifest.a87b7272451f2032a50d10488649cef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ec3782d19ce56e774cc5c73c6942d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b55a34a456cf6149d20",
+    "url": "/static/js/2.585f720c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.585f720c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a6d8dd06f88c4a6e6b",
+    "url": "/static/js/main.6cf64ef2.chunk.js"
+  },
+  {
+    "revision": "0b5383bc41c0ce1f85b7",
+    "url": "/static/js/runtime-main.78be2304.js"
+  }
+]);
