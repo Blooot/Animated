504edd88651b7cbd151caba26d3190c14df6e007
blooot <dalehamilton@trentu.ca>
2020-08-14 12:14:35 -0700

    Updates

---
+++ precache-manifest.265a7d34b47f0e1ffaa3d85f45aa813b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b9e40f15a53b0da1d52a3a63667284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ee4d20266d1c92b7e1d",
+    "url": "/static/css/main.0c652f3e.chunk.css"
+  },
+  {
+    "revision": "e9c927945da2bf259619",
+    "url": "/static/js/2.c274f2cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c274f2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee4d20266d1c92b7e1d",
+    "url": "/static/js/main.6d0b559d.chunk.js"
+  },
+  {
+    "revision": "0b5383bc41c0ce1f85b7",
+    "url": "/static/js/runtime-main.78be2304.js"
+  },
+  {
+    "revision": "f0fae472e8c3adb9a5eb9575bd7c9ae7",
+    "url": "/static/media/TestImage.f0fae472.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.dalehamilton.dev/precache-manifest.1e6032ec026621c7e1f1d8a4d0b390a8.js"
+  "/precache-manifest.265a7d34b47f0e1ffaa3d85f45aa813b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.dalehamilton.dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
